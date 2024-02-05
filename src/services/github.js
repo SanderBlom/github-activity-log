ba0182cac0a2c5dc532c8e3974f3d41bb3ee76ef
Sander Blomvågnes <sander@blomvag.no>
2024-02-05 17:05:20 +0100

    feat: Added merge status to PR response

--- src/services/github.js
+++ src/services/github.js
@@ -1,10 +1,23 @@
 import { getRedisClient } from '../utils/redisClient.js';
 
+/**
+ * @typedef {Object} TransformedPullRequest
+ * @property {string} title - The title of the pull request.
+ * @property {string} url - The HTML URL of the pull request.
+ * @property {string} createdAt - The creation date of the pull request.
+ * @property {string} user - The GitHub username of the pull request creator.
+ * @property {string} state - The state of the pull request (e.g., open, closed).
+ * @property {string} repoName - The name of the repository.
+ * @property {string} orgOrUserName - The GitHub username or organization name of the repository owner.
+ * @property {boolean} merged - A flag indicating whether the pull request has been merged.
+ */
+
+
 
 /**
  * Fetches all pull requests (open and closed) for a given GitHub username from Redis cache or GitHub API for the last 3 months.
  * @param {string} username - The GitHub username.
- * @returns {Promise<Array<{title: string, url: string, createdAt: string, user: string, state: string, repoName: string, orgOrUserName: string}>>} A promise that resolves to an array of pull request objects.
+ * @returns {Promise<TransformedPullRequest[]>} A promise that resolves to an array of pull request objects.
  */
 async function fetchAllPullRequests(username) {
     const client = await getRedisClient();
@@ -19,7 +32,7 @@ async function fetchAllPullRequests(username) {
         const url = `https://api.github.com/search/issues?q=is:pr+author:${username}+created:>=${threeMonthsAgo}`;
         const response = await fetch(url);
         const data = await response.json();
-
+        /** @type {TransformedPullRequest} */   
         const pullRequests = await transformPRData(data.items);
 
         // Store the fetched data in Redis with a TTL of 12 hours
@@ -66,14 +79,17 @@ function getThreeMonthsAgoDate() {
 /**
  * This is a generic transformation function that might be applied within both getOpenPullRequests and getClosedPullRequests
  * @param {Array} pullRequests - An array of pull request objects from the GitHub API.
- * @returns {Promise<Array<{title: string, url: string, createdAt: string, user: string, state: string, repoName: string, orgOrUserName: string}>>} A promise that resolves to an array of pull request objects, including the repository name and the organization or username.
+ * @returns {TransformedPullRequest} A promise that resolves to an array of pull request objects, including the repository name, the organization or username, and a merged flag.
  */
 async function transformPRData(pullRequests) {
     return pullRequests.map(pr => {
         // Extracting the repository name and organization/username from the repository_url
         const repoUrlParts = pr.repository_url.split('/');
         const repoName = repoUrlParts.pop(); // The last part is the repo name
-        const orgOrUserName = repoUrlParts.pop(); // The second last part is the organization or username
+        const orgOrUserName = repoUrlParts.pop(); // The second last part is the organization/username or username
+
+        // Determining the merged status
+        const merged = pr.pull_request.merged_at !== null;
 
         return {
             title: pr.title,
@@ -82,7 +98,8 @@ async function transformPRData(pullRequests) {
             user: pr.user.login,
             state: pr.state,
             repoName: repoName,
-            orgOrUserName: orgOrUserName
+            orgOrUserName: orgOrUserName,
+            merged: merged
         };
     });
 }
